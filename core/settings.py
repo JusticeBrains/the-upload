efe3d19bb1d6490f8d0e9a12481cccc49c5b1db4
JusticeBrains <justiceduodu14@gmail.com>
2022-10-16 22:00:58 +0100

    staticurl changed

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/theme/static/'
+STATIC_URL = '/theme/staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'theme/staticfiles')
 
