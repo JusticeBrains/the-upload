473c45a7f63d7003fb5d8568b153e2d872e5222b
JusticeBrains <justiceduodu14@gmail.com>
2022-10-16 13:38:49 +0100

    allowed host added

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-upload-production.up.railway.app']
 
 
 # Application definition
